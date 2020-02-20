3ee0326aa6924150831abb80875fca5bf5f757ab
Wyane <Wyane.Yia@gmail.com>
2020-02-20 10:08:55 +0900

    Update

--- theme/myui.php
+++ theme/myui.php
@@ -45,7 +45,7 @@
         .operatediv{position:absolute;border:1px #CCCCCC;background-color:#FFFFCC;z-index:2;}
         .operatediv div{margin:16px}
         .operatediv_close{position:absolute;right:3px;top:3px;}
-	.footer{text-align: center;color:#f7f7f9;}
+	.footer{text-align:center;color:#f7f7f9;}
         .readme{padding:8px;background-color:#fff;}
         #readme{padding:20px;text-align:left}
         @media only screen and (max-width:480px){
@@ -493,12 +493,11 @@
 	</div>
 <?php   }
     } ?>
-    <font style="text-align:center;color:#f7f7f9><?php echo date("Y-m-d H:i:s")." ".getconstStr('Week')[date("w")]." ".$_SERVER['REMOTE_ADDR'];?></font>
-</body>
-<footer>
+    <footer style="text-align:center;color:#f7f7f9>
        <p><?php echo date("Y-m-d H:i:s")." ".getconstStr('Week')[date("w")]." ".$_SERVER['REMOTE_ADDR'];?></p>
        <p>Powered by <a href="https://huaien.co/" target="_blank">Wyane`s Blog</a>. Since 2010.</p>
-</footer>
+    </footer>
+</body>
 
 
 <?php if ($head||$readme) { ?><link rel="stylesheet" href="//unpkg.zhimg.com/github-markdown-css@3.0.1/github-markdown.css">
